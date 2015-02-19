1b172d0ebdf76163ace99b78c1a7f8dd4758196a
Chris Loew <cloew123@gmail.com>
2015-02-19 00:31:33 -0700

    Added setup script credentials

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='pbf_project',
-      version='.1',
+      version='0.1.0',
       description="Programmer's Best Friend Utility Extension for pbf_project",
-      author='', # Add your name here
-      author_email='', # Add your e-mail here
-      packages=['pbf_project', 'pbf_project.Commands', 'pbf_project.templates'],
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      packages=['pbf_project', 'pbf_project.Commands', 'pbf_project.helpers'],
       #package_data = {'pbf_project.templates':[]}, # Add template files
      )
